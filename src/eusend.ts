6016cf2877281d6ee323937362a78e804ef29028
Sivert Bjørnerås <sivertmail@gmail.com>
2026-08-25 12:56:36 +0200

    fix: synchronize SDK user agent version

--- src/eusend.ts
+++ src/eusend.ts
@@ -10,7 +10,7 @@ import { Broadcasts } from './broadcasts'
 import { Suppressions } from './suppressions'
 
 const DEFAULT_BASE_URL = 'https://api.eusend.dev'
-const SDK_VERSION = '0.8.0'
+const SDK_VERSION = '0.11.2'
 
 export interface EusendOptions {
   baseUrl?: string
